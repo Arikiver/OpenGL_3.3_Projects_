15e1dc9462f084fb4516658cba33ac1c19768f76
Aaradhya Bhatiya <Bhatiyaaaradhya@gmail.com>
2024-08-30 16:18:56 +0530

    stuff

--- DDA.cpp
+++ DDA.cpp
@@ -5,23 +5,23 @@
 #include <cmath>
 
 const char* vertexShaderSource = R"glsl(
-    #version 330 core
-    layout(location = 0) in vec2 aPos;
-    
-    void main()
-    {
-        gl_Position = vec4(aPos, 0.0, 1.0);
-    }
+   #version 330 core
+   layout(location = 0) in vec2 aPos;
+   
+   void main()
+   {
+       gl_Position = vec4(aPos, 0.0, 1.0);
+   }
 )glsl";
 
 const char* fragmentShaderSource = R"glsl(
-    #version 330 core
-    out vec4 FragColor;
-    
-    void main()
-    {
-        FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
-    }
+   #version 330 core
+   out vec4 FragColor;
+   
+   void main()
+   {
+       FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
+   }
 )glsl";
 
 std::vector<float> points; // Store points in this vector
